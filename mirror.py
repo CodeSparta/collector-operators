a6cb4bdb43568f0d9c829c2f9bd2a424a8009a22
usrbinkat <kat@braincraft.io>
2020-07-18 14:35:41 -0600

    nested buildah

--- mirror.py
+++ mirror.py
@@ -311,14 +311,12 @@ def CreateCatalogImageAndPushToLocalRegistry():
 
   content = templateFile.render(manifestPath=os.path.basename(manifest_root_dir))
 
-  #dockerFile = os.path.join(content_root_dir, 'Dockerfile')
-  dockerFile = os.path.join(manifest_root_dir, 'Dockerfile')
+  dockerFile = os.path.join(content_root_dir, 'Dockerfile')
 
   with open(dockerFile, "w") as dockerfile:
     dockerfile.write(content)
 
-  #cmd_args = "buildah bud --isolation=chroot --format docker -f {} -t {}".format(content_root_dir, image_url)
-  cmd_args = "cd / && buildah bud --isolation=chroot --format docker -f {} -t {}".format(manifest_root_dir, image_url)
+  cmd_args = "cd / && buildah bud --isolation=chroot --format docker -f {} -t {}".format(content_root_dir, image_url)
   subprocess.run(cmd_args, shell=True, check=True)
 
   print("Pushing catalog image to offline registry...")
