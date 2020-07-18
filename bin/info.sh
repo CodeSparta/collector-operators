f6afc8311b94c690300a5ab907e1da7302112989
usrbinkat <kat@braincraft.io>
2020-07-18 12:58:09 -0600

    add info close

---
+++ bin/info.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+info () {
+bundleDu="$(du -h /root/deploy/bundle/koffer-bundle.collector-operators.tar | awk '{print $1}' | head -n 1)"
+bundlePath="$(ls /root/deploy/bundle/koffer-bundle.collector-operators.tar | head -n 1 | sed 's/root\/deploy/tmp\/platform/')"
+cat <<EOF
+
+  Koffer Bundle Complete: ${bundleDu} ${bundlePath}
+
+  Next Steps:
+    - Move to cloudctl deployment services bastion \`/tmp\`
+    - Unpack bundle into image registry path via cmd:
+
+        \`tar xv -f /tmp/koffer-bundle.collector-operators.tar -C /root/deploy/mirror\`
+
+EOF
+}
+info
