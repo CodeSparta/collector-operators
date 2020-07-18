379487ad63a8b05d9dbc0e0a9e484c2d836824be
usrbinkat <kat@braincraft.io>
2020-07-18 13:40:35 -0600

    nested buildah

--- ocp4-custom-catalog-mirror/podman.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-sudo podman run -it --rm \
-    --entrypoint bash \
-    --volume $(pwd):/root/koffer:z \
-    --volume /tmp/platform/secrets/docker/quay.json:/root/.docker/config.json:ro \
-  docker.io/containercraft/koffer:nightlies
-
