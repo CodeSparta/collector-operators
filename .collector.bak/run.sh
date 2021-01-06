7b8c8a751c47f29a6e6896f8ae655e9e05228cc3
usrbinkat <usrbinkat@braincraft.io>
2021-01-06 10:20:24 -0800

    working run
    Signed-off-by: usrbinkat <usrbinkat@braincraft.io>

--- .collector.bak/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-./redhat.py \
---catalog-version 1.0.0 \
---authfile /root/.docker/config.json \
---registry-olm localhost:5000 \
---registry-catalog localhost:5000 \
---operator-file ./operators.list \
---output /root/platform/mirror/config/operators \
---icsp-scope=namespace
--- .collector.bak/tools/dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir -p ${HOME}/bundle
-sudo podman run -it --rm \
-    --privileged --device /dev/fuse \
-    --entrypoint bash --name koffer -h koffer\
-    --volume ${HOME}/bundle:/root/bundle:z \
-    --volume ${HOME}/.docker:/root/.docker:z \
-    --volume $(pwd):/root/koffer:z \
-  quay.io/cloudctl/koffer
