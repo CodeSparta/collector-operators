1ce2c2fbe7e73c7d0c88a64d381dbd00f9b7638f
usrbinkat <usrbinkat@braincraft.io>
2021-01-05 20:03:17 -0800

    wip
    Signed-off-by: usrbinkat <usrbinkat@braincraft.io>

--- collector/tools/dev.sh
+++ collector/tools/dev.sh
@@ -1,16 +1,44 @@
-#!/bin/bash -x
+#!/bin/bash
 
-project="collector-openshift"
-clear && mkdir -p /tmp/bundle
+# For now, koffer operator collector must be run as root
+if (( $EUID != 0  )); then
+  echo ">> Please run as root"
+  exit 1
+else
+# For now, koffer executes index image startup and custom image build on host
+# ansible connection over ssh required from koffer onto host
+  ready=$(ssh root@10.88.0.1 whoami ; echo $?)
+  if [[ ! ${ready} == 0 ]]; then
+        echo ">> Host ssh connection discovered successfully"
+  else
+        echo ">> Host ssh connection not found"
+        echo ">> Configuring host for cni ssh connection"
+        [[ -f ${HOME}/.ssh/id_rsa ]] \
+        || ssh-keygen -f ${HOME}/.ssh/id_rsa -t rsa -N ''
+        cat ${HOME}/.ssh/id_rsa.pub >> ${HOME}/.ssh/authorized_keys
+        chmod 0644 ${HOME}/.ssh/authorized_keys
+  fi
+fi
 
-sudo podman run -it --rm --pull always \
-    --name ${project} -h ${project} \
-    --volume $(pwd):/root/koffer:z \
-    --volume /tmp/bundle:/root/bundle:z \
-    --workdir /root/koffer --entrypoint bash \
-    --volume ${HOME}/.docker:/root/.docker:ro \
-    --volume ${HOME}/.gitconfig:/root/.gitconfig:z \
-  docker.io/codesparta/koffer:latest
+#ansible_user="$(whoami)"
+project="collector-operators"
 
-#   --volume ${HOME}/.aws:/root/.aws:z \
-#   --volume ${HOME}/.docker:/root/.docker:z \
+echo ">> Pulling quay.io/cloudctl/koffer"
+podman pull quay.io/cloudctl/koffer:latest
+
+rm -rf /tmp/.ssh
+cp -rf ~/.ssh /tmp/.ssh
+mkdir -p ${HOME}/{.aws,.docker,bundle}
+
+#clear
+echo ">>  Starting Koffer"
+podman run -it --rm --entrypoint bash \
+    --workdir ${HOME}/koffer                   \
+    --name ${project} -h ${project}            \
+    --volume  $(pwd):/root/koffer:z            \
+    --volume  /tmp/.ssh:/root/.ssh:z           \
+    --volume  ${HOME}/.aws:/root/.aws:z        \
+    --volume  ${HOME}/.docker:/root/.docker:z  \
+    --volume  ${HOME}/.bashrc:/root/.bashrc:z  \
+    --volume  ${HOME}/bundle:/root/bundle:z    \
+  quay.io/cloudctl/koffer:latest
