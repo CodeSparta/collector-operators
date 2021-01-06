9fe555f46784a5ce50ddef922f0d5e5db61800ca
usrbinkat <usrbinkat@braincraft.io>
2021-01-06 10:20:24 -0800

    working run
    Signed-off-by: usrbinkat <usrbinkat@braincraft.io>

--- .collector.bak/test.sh
+++
@@ -1,1 +0,0 @@
-./redhat.py --catalog-version 1.0.0 --authfile /root/.docker/config.json --registry-olm localhost:5000 --registry-catalog localhost:5000 --operator-file ./redhat.list --output /root/platform/mirror/operators/redhat --icsp-scope=namespace
