ba7fe4d07b39374c63cd76cd127d766a50bd24b4
usrbinkat <usrbinkat@braincraft.io>
2021-01-26 12:33:18 -0800

    enable wall-e opm wrapper
    Signed-off-by: usrbinkat <usrbinkat@braincraft.io>

--- collector/tools/dev.sh
+++ collector/tools/dev.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 run () {
-# For now, koffer operator collector must be run as root
-if (( $EUID != 0  )); then
-  echo ">> Please run as root"
-  exit 1
-else
-# For now, koffer executes index image startup and custom image build on host
-# ansible connection over ssh required from koffer onto host
-  ready=$(ssh root@10.88.0.1 whoami ; echo $?)
-  if [[ ! ${ready} == 0 ]] && \
-     [[ -f "/root/.ssh/id_rsa" ]]; then
-        echo ">> Host ssh connection discovered successfully"
-  else
-        echo ">> Host ssh connection not found"
-        echo ">> Configuring host for cni ssh connection"
-        [[ -f ${HOME}/.ssh/id_rsa ]] \
-        || ssh-keygen -f ${HOME}/.ssh/id_rsa -t rsa -N ''
-        cat ${HOME}/.ssh/id_rsa.pub >> ${HOME}/.ssh/authorized_keys
-        chmod 0644 ${HOME}/.ssh/authorized_keys
-  fi
-fi
+clear
 
 #ansible_user="$(whoami)"
 project="collector-operators"
@@ -28,23 +9,17 @@ echo ">> Pulling quay.io/cloudctl/koffer"
 podman pull quay.io/cloudctl/koffer:extra
 
 rm -rf /tmp/koffer
-mkdir -p /tmp/koffer
-mkdir -p ${HOME}/{bundle,operators}
-cp -rf ~/.ssh /tmp/koffer/.ssh
-cp -rf ~/.aws /tmp/koffer/.aws
+mkdir -p /tmp/koffer ${HOME}/{.docker,bundle,operators}
 cp -rf ~/.docker /tmp/koffer/.docker
-mkdir -p /tmp/koffer/{.ssh,.docker,.aws}
 
 #clear
 echo ">>  Starting Koffer"
 podman run -it --rm --entrypoint bash \
-    --privileged \
-    --device /dev/fuse \
-    --workdir ${HOME}/koffer                       \
     --name ${project} -h ${project}                \
+    --privileged --device /dev/fuse                \
     --volume  $(pwd):/root/koffer:z                \
-    --volume  /tmp/koffer/.docker:/root/.docker:z  \
     --volume  ${HOME}/bundle:/root/bundle:z        \
+    --volume  /tmp/koffer/.docker:/root/.docker:z  \
   quay.io/cloudctl/koffer:extra
 }
 
