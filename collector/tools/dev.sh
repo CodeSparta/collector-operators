c95fd622cc65b99a1fccb22c875b63d306e3a62a
usrbinkat <usrbinkat@braincraft.io>
2021-02-21 02:17:40 +0000

    building multi-catalog capability

--- collector/tools/dev.sh
+++ collector/tools/dev.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
 run () {
-clear
-
-#ansible_user="$(whoami)"
 project="collector-operators"
-
-echo ">> Pulling quay.io/cloudctl/koffer"
-podman pull quay.io/cloudctl/koffer:extra
-
+#echo ">> Pulling quay.io/cloudctl/koffer"
+#podman pull quay.io/cloudctl/koffer:extra
 rm -rf /tmp/koffer
 mkdir -p /tmp/koffer ${HOME}/{.docker,bundle,operators}
-cp -rf ~/.docker /tmp/koffer/
-
-#clear
 echo ">>  Starting Koffer"
 sudo podman run -it --rm --entrypoint bash \
-    --name ${project} -h ${project}                \
-    --privileged --device /dev/fuse                \
-    --volume  $(pwd):/root/koffer:z                \
-    --volume  /tmp/koffer/.docker:/root/.docker:z  \
-    --volume  ${HOME}/bundle:/root/bundle:z        \
+    --name ${project} -h ${project}                        \
+    --privileged --device /dev/fuse                        \
+    --volume  $(pwd):/root/koffer:z                        \
+    --volume  ${HOME}/bundle:/root/bundle:z                \
+    --volume  ${HOME}/.docker:/root/.docker:z              \
+    --volume  $(pwd)/koffer.yml:/root/.koffer/config.yml:z \
   quay.io/cloudctl/koffer:extra
 }
 
-run
+clear && run
---
+++ roles/collector/README.md
@@ -0,0 +1,34 @@
+Collector Operators | Koffer Collector Plugin
+=========
+
+This role gathers kubernetes operator dependencies & builds custom operator catalog index images.
+
+Requirements
+------------
+
+This operators koffer collector plugin is designed to run in the `CloudCtl/koffer:extra` container which meets all dependencies. 
+
+Role Variables
+--------------
+
+A description of the settable variables for this role should go here, including any variables that are in defaults/main.yml, vars/main.yml, and any variables that can/should be set via parameters to the role. Any variables that are read from other roles and/or the global scope (ie. hostvars, group vars, etc.) should be mentioned here as well.
+
+Example Playbook
+----------------
+
+Including an example of how to use your role (for instance, with variables passed in as parameters) is always nice for users too:
+
+    - hosts: servers
+      roles:
+         - { role: username.rolename, x: 42 }
+
+License
+-------
+
+GNUv3
+
+Author Information
+------------------
+
+Written by the CodeSparta developer team & contributors.
+https://codectl.io
